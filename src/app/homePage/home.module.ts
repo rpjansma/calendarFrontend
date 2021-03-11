9caef94486dc2377619d24e73a374beba0f34293
Raul Paes <rpjansma@gmail.com>
2021-03-10 21:13:00 -0300

    Username and password validation. Validation message component.

--- src/app/homePage/home.module.ts
+++ src/app/homePage/home.module.ts
@@ -1,7 +1,16 @@
 import { SignInComponent } from './signIn/signin.component';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from './../shared/vmessage/vmessage.module';
+
 
 @NgModule({
   declarations: [SignInComponent],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    VMessageModule
+    ],
 })
 export class HomeModule {}
---
+++ src/app/shared/vmessage/vmessage.module.ts
@@ -0,0 +1,11 @@
+import { VMessageComponent } from './vmessage.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [VMessageComponent],
+  exports: [VMessageComponent]
+})
+export class VMessageModule { }
