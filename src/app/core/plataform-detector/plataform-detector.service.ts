e1aefc3adaebc0b79b994e12e980aadc9275e3ab
Raul Paes <rpjansma@gmail.com>
2021-03-11 22:32:06 -0300

    Plataform Detector Service implemented to identify Client-side renderization

---
+++ src/app/core/plataform-detector/plataform-detector.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from "@angular/common";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlatformDetector {
+  constructor(@Inject(PLATFORM_ID) private platformId: string) {};
+
+  isBrowser() {
+    return isPlatformBrowser(this.platformId);
+  }
+}
--- src/app/homePage/signIn/signin.component.ts
+++ src/app/homePage/signIn/signin.component.ts
@@ -1,19 +1,21 @@
-import { AuthService } from './../../core/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { PlatformDetector } from './../../core/plataform-detector/plataform-detector.service';
+import { AuthService } from './../../core/auth/auth.service';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
 
 @Component({
   templateUrl: './signin.component.html',
 })
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
+  @ViewChild('usernameInput') usernameInput: ElementRef<HTMLInputElement>;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private platformDetectorService: PlatformDetector
   ) {}
 
   ngOnInit(): void {
@@ -27,10 +29,15 @@ export class SignInComponent implements OnInit {
     const username = this.loginForm.get('username')?.value;
     const password = this.loginForm.get('password')?.value;
 
-    this.authService.authenticate(username, password).subscribe( () => this.router.navigate(['calendar']), error => {
-      console.log(error);
-      this.loginForm.reset()
-    })
-
+    this.authService.authenticate(username, password).subscribe(
+      () => this.router.navigate(['calendar']),
+      (error) => {
+        console.log(error);
+        this.loginForm.reset();
+        this.platformDetectorService.isBrowser() &&
+          this.usernameInput.nativeElement.focus();
+        alert('Invalid username or password');
+      }
+    );
   }
 }
